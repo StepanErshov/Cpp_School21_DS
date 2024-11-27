d73a0e411f799f66916a1e1d81d1506b6bc01f9e
StepanErshov <1132227880@pfur.ru>
2024-11-27 13:19:00 +0300

    meow

--- DS/test.py
+++ DS/test.py
@@ -3,7 +3,7 @@ import numpy as np
 from tensorflow import keras 
 from sklearn.metrics import accuracy_score
 
-model = tf.keras.Sequential([keras.layers.Dense(units = 1, input_shape =[1])]) 
+model = tf.keras.Sequential([keras.layers.Dense(units = 1, input_shape = [1])]) 
 model.compile(optimizer = 'sgd', loss = 'mean_squared_error')
 
 xs = np.array([- 1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype = float) 
@@ -12,3 +12,4 @@ ys = np.array([- 4.0, 1.0, 6.0, 11.0, 16.0, 21.0], dtype = float)
 model.fit(xs, ys, epochs = 500)
 
 print(model.predict(x = np.array([10.0]))) 
+x = list(map(int, input().strip().split(' ')))
